4a6c10e7783c78a397302a55eb34da59cabfb300
SHK-FIX <schuhmacher36@googlemail.com>
2026-09-08 10:33:32 +0100

    Cache photo PDF export offline

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='shkfix-aufmass-v4';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./pdf-unicode.js','./photo-store.js','./photo-ui.js','./manifest.json'];
+const CACHE='shkfix-aufmass-v5';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./pdf-unicode.js','./photo-store.js','./photo-ui.js','./pdf-photos.js','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;}).catch(()=>caches.match('./index.html')))));
