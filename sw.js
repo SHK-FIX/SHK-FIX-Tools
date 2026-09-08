e99290e35190b12dfd85ef51524dc04e0af9242d
SHK-FIX <schuhmacher36@googlemail.com>
2026-09-08 14:10:40 +0200

    Cache UX and fire protection update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='shkfix-aufmass-v6';
+const CACHE='shkfix-aufmass-v7';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./module-v2.js','./pdf-unicode.js','./photo-store.js','./photo-ui.js','./pdf-photos.js','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
