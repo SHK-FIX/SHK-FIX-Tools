70d7dd8ac6cf33847da97f18561a357a7a0609df
SHK-FIX <schuhmacher36@googlemail.com>
2026-09-08 14:43:37 +0200

    Cache simplified Aufmass workflow

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='shkfix-aufmass-v7';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./module-v2.js','./pdf-unicode.js','./photo-store.js','./photo-ui.js','./pdf-photos.js','./manifest.json'];
+const CACHE='shkfix-aufmass-v8';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./module-v2.js','./workflow-v4.js','./pdf-unicode.js','./photo-store.js','./photo-ui.js','./pdf-photos.js','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;}).catch(()=>caches.match('./index.html')))));
